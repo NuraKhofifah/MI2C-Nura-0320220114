a7f88836def720da63751136aa23f7f803da0a48
student <nurawartami@gmail.com>
2024-06-14 14:35:24 +0700

    first commit

---
+++ src/main/java/com/astratech/_C_Nura_0320220114/Application.java
@@ -0,0 +1,13 @@
+package com.astratech._C_Nura_0320220114;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/astratech/_C_Nura_0320220114/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astratech._C_Nura_0320220114;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
